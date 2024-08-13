1bd9b4c393f1d30adb1b024a1c932ba3318e3a82
beauty-cloud <sengkleh09@gmail.com>
2024-08-13 15:37:31 +0700

    ubah kabeh

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,3 +7,23 @@ document.addEventListener('scroll', () => {
         header.classList.remove('scrolled');
     }
 });
+
+
+//  window.addEventListener('scroll', function () {
+//             let sections = document.querySelectorAll('section');
+//             let navLinks = document.querySelectorAll('.navbar-nav .nav-link');
+
+//             let scrollPosition = window.scrollY || document.documentElement.scrollTop;
+
+//             sections.forEach(section => {
+//                 if (scrollPosition >= section.offsetTop - 70 && scrollPosition < section.offsetTop + section.offsetHeight - 70) {
+//                     let currentId = section.getAttribute('id');
+//                     navLinks.forEach(navLink => {
+//                         navLink.classList.remove('active');
+//                         if (navLink.getAttribute('data-section') === currentId) {
+//                             navLink.classList.add('active');
+//                         }
+//                     });
+//                 }
+//             });
+//         });
